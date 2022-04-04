44ecbf71c0df6e42bb71c2f8bed4aa834712c762
Xinas <vixus01@gmail.com>
2022-04-04 12:11:47 +0200

    npc_ship_place

--- statki/statki.py
+++ statki/statki.py
@@ -14,7 +14,9 @@ def generate_map():  # generacja mapy do gry
         else:
             wymiar_tab = [[' '] * wymiar for i in range(wymiar)]
             map_destro = [[' '] * wymiar for i in range(wymiar)]
-            return wymiar_tab, map_destro, wymiar
+            map_destro2 = [[' '] * wymiar for i in range(wymiar)]
+            map_2 = [[' '] * wymiar for i in range(wymiar)]
+            return wymiar_tab, map_destro, map_destro2, map_2, wymiar
 
 
 def place_warship_2():  # ustawianie statków
@@ -31,25 +33,22 @@ def place_warship_2():  # ustawianie statków
             if x == dimension and not (x == dimension and y == 1) and not (x == dimension and y == dimension):
                 print("prawa granica mapy nie można ustawic statku skierowanego w prawo")
                 print("podaj nowe pozycje")
-                place_warship_2(map_player, dimension)
+                place_warship_2()
             elif x == dimension and (x == dimension and y == 1) and not (x == dimension and y == dimension):
                 print(
                     "prawy gorny rog mapy nie można ustawic statku skierowanego w prawo lub gore")
                 print("podaj nowe pozycje")
-                place_warship_2(map_player, dimension)
+                place_warship_2()
             elif x == dimension and not (x == dimension and y == 1) and (x == dimension and y == dimension):
                 print(
                     "prawy dolny rog mapy nie można ustawic statku skierowanego w prawo lub dol")
                 print("podaj nowe pozycje")
-                place_warship_2(map_player, dimension)
+                place_warship_2()
             else:
                 map_warship_destroy_check[x-1][y-1] = ship_count
                 map_warship_destroy_check[x-1][y] = ship_count
                 map_player[x-1][y-1] = "o"
                 map_player[x-1][y] = "o"
-                # map_warship_destroy_check[x-1][y-1] = ship_count
-                # map_warship_destroy_check[x-1][y] = ship_count
-
                 ship_count += 1
 
                 return ship_count
@@ -58,56 +57,66 @@ def place_warship_2():  # ustawianie statków
                 print(
                     "prawy dolny rog mapy nie można ustawic statku skierowanego w prawo lub dol")
                 print("podaj nowe pozycje")
-                place_warship_2(map_player, dimension)
+                place_warship_2()
             elif y == dimension and not (y == dimension and x == 1) and not (y == dimension and x == dimension):
                 print("dolna granica mapy nie mozna ustawic statku w dol")
                 print("podaj nowe pozycje")
-                place_warship_2(map_player, dimension)
+                place_warship_2()
             elif y == dimension and (y == dimension and x == 1) and not (y == dimension and x == dimension):
                 print("lewy dolny rog mapy nie mozna ustawic statku w dol i w lewo")
                 print("podaj nowe pozycje")
-                place_warship_2(map_player, dimension)
+                place_warship_2()
             else:
+                map_warship_destroy_check[x-1][y-1] = ship_count
+                map_warship_destroy_check[x][y-1] = ship_count
                 map_player[x-1][y-1] = "o"
                 map_player[x][y-1] = "o"
-                return map_player
+                ship_count += 1
+                return ship_count
 
         case "lewo":
             if x == 1 and not (x == 1 and y == dimension) and not (x == 1 and y == 1):
                 print("lewa granica mapy nie można ustawic statku skierowanego w lewo")
                 print("podaj nowe pozycje")
-                place_warship_2(map_player, dimension)
+                place_warship_2()
             elif x == 1 and (x == 1 and y == dimension) and not (x == 1 and y == 1):
                 print(
                     "lewy dolny rog mapy nie można ustawic statku skierowanego w lewo i dol")
                 print("podaj nowe pozycje")
-                place_warship_2(map_player, dimension)
+                place_warship_2()
             elif x == 1 and not (x == 1 and y == dimension) and (x == 1 and y == 1):
                 print(
                     "lewy gorny rog mapy nie można ustawic statku skierowanego w lewo i dol")
                 print("podaj nowe pozycje")
-                place_warship_2(map_player, dimension)
+                place_warship_2()
             else:
+                map_warship_destroy_check[x-1][y-1] = ship_count
+                map_warship_destroy_check[x-1][y-2] = ship_count
                 map_player[x-1][y-1] = "o"
                 map_player[x-1][y-2] = "o"
-                return map_player
+                ship_count += 1
+                return ship_count
         case "gora":
             if x == 1 and not(x == 1 and y == dimension) and not (x == 1 and y == 1):
                 print("gorna granica mapy nie mozna ustawic statku w gore")
                 print("podaj nowe pozycje")
-                place_warship_2(map_player, dimension)
-            elif x == 1 and (x == 1 and y == dimension) and not (x == 1 and y == 1):
+                place_warship_2()
+            elif x == 1 and y == 1:
                 print("lewy gorny rog mapy nie mozna ustawic statku w gore i lewo")
                 print("podaj nowe pozycje")
-                place_warship_2(map_player, dimension)
-            elif x == 1 and not (x == 1 and y == dimension) and (x == 1 and y == 1):
+                place_warship_2()
+            elif x == dimension and (x == dimension and y == 1) and not (x == dimension and y == dimension):
+
                 print("prawy gorny rog mapy nie mozna ustawic statku w gore i prawo")
                 print("podaj nowe pozycje")
-                place_warship_2(map_player, dimension)
+                place_warship_2()
             else:
+                map_warship_destroy_check[x-1][y-1] = ship_count
+                map_warship_destroy_check[x-2][y-1] = ship_count
                 map_player[x-1][y-1] = "o"
                 map_player[x-2][y-1] = "o"
-                return map_player
+                ship_count += 1
+                return ship_count
 
 
 def warship_player_shot():
@@ -115,28 +124,95 @@ def warship_player_shot():
     print("Podaj pozycje x i y strzału")
     x = int(input())
     y = int(input())
-    if map_player[x-1][y-1] == "o":
+    if map_npc[x-1][y-1] == "o":
         print("statek trafiony")
-        map_player[x-1][y-1] = "x"
-        map_warship_destroy_check[x-1][y-1] = "x"
+        map_npc[x-1][y-1] = "x"
+        map_destro_npc[x-1][y-1] = "x"
         for num in range(dimension):
-            cnt = map_warship_destroy_check[num].count(0)
+            cnt = map_destro_npc[num].count(0)
             licznik = licznik+cnt
         if licznik == 0:
             print('statek zniszczony')
-        return map_player
+        return map_npc
     else:
-        map_player[x-1][y-1] = "m"
+        map_npc[x-1][y-1] = "m"
         print("pudlo")
-        return map_player
+        return map_npc
+
+    # npc_ship_counter = 0
+    # x = random.randint(0, dimension)
+    # y = random.randint(0, dimension)
+    # pos = random.randint(0, 4)
 
 
-map_player, map_warship_destroy_check, dimension = generate_map()
+def npc_ship_place_2():  # ustawianie statków
+    npc_ship_counter = 0
+    x = random.randint(0, dimension)
+    y = random.randint(0, dimension)
+    pos = random.randint(0, 4)
+    match pos:
+        case 1:
+            if x == dimension and not (x == dimension and y == 1) and not (x == dimension and y == dimension):
+                npc_ship_place_2()
+            elif x == dimension and (x == dimension and y == 1) and not (x == dimension and y == dimension):
+                npc_ship_place_2()
+            elif x == dimension and not (x == dimension and y == 1) and (x == dimension and y == dimension):
+                npc_ship_place_2()
+            else:
+                map_destro_npc[x-1][y-1] = npc_ship_counter
+                map_destro_npc[x-1][y] = npc_ship_counter
+                map_npc[x-1][y-1] = "o"
+                map_npc[x-1][y] = "o"
+                npc_ship_counter += 1
+                return npc_ship_counter
+        case 2:
+            if y == dimension and not (y == dimension and x == 1) and (y == dimension and x == dimension):
+                npc_ship_place_2()
+            elif y == dimension and not (y == dimension and x == 1) and not (y == dimension and x == dimension):
+                npc_ship_place_2()
+            elif y == dimension and (y == dimension and x == 1) and not (y == dimension and x == dimension):
+                npc_ship_place_2()
+            else:
+                map_destro_npc[x-1][y-1] = npc_ship_counter
+                map_destro_npc[x][y-1] = npc_ship_counter
+                map_npc[x-1][y-1] = "o"
+                map_npc[x][y-1] = "o"
+                return npc_ship_counter
+
+        case 3:
+            if x == 1 and not (x == 1 and y == dimension) and not (x == 1 and y == 1):
+                npc_ship_place_2()
+            elif x == 1 and (x == 1 and y == dimension) and not (x == 1 and y == 1):
+                npc_ship_place_2()
+            elif x == 1 and not (x == 1 and y == dimension) and (x == 1 and y == 1):
+                npc_ship_place_2()
+            else:
+                map_destro_npc[x-1][y-1] = npc_ship_counter
+                map_destro_npc[x-1][y-2] = npc_ship_counter
+                map_npc[x-1][y-1] = "o"
+                map_npc[x-1][y-2] = "o"
+                return npc_ship_counter
+        case 4:
+            if x == 1 and not(x == 1 and y == dimension) and not (x == 1 and y == 1):
+                npc_ship_place_2()
+            elif x == 1 and y == 1:
+                npc_ship_place_2()
+            elif x == dimension and (x == dimension and y == 1) and not (x == dimension and y == dimension):
+                npc_ship_place_2()
+            else:
+                map_destro_npc[x-1][y-1] = npc_ship_counter
+                map_destro_npc[x-2][y-1] = npc_ship_counter
+                map_npc[x-1][y-1] = "o"
+                map_npc[x-2][y-1] = "o"
+                return npc_ship_counter
+
+
+map_player, map_warship_destroy_check, map_destro_npc, map_npc, dimension = generate_map()
 
 ship_amount = place_warship_2()
-
+npc_ship_amount = npc_ship_place_2()
 for num in range(dimension):
-    print(map_player[num])
+    print(map_npc[num])
 
 # for num in range(dimension):
 #     print("destro czek", map_warship_destroy_check[num])
